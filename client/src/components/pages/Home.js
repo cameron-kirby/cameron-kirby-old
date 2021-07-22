185e9d021f61f2781404c4d9eeafbe2c39ad7c10
Cameron Kirby <cameronevankirby@gmail.com>
2021-07-22 03:14:27 -0500

    :green_heart: Linting errors

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import anime from 'animejs'
 
 // COMPONENTS
 import Blobtext from '../pieces/Blobtext'
